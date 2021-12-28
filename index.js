357e7bd77f160dba5abf852d79407238a679a891
avneesh0612 <avneeshagarwal0612@gmail.com>
2021-12-28 14:01:44 +0530

    port error fix?

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use(router);
 
 app.listen(process.env.PORT || 8000, () => {
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Server is running");
 });
